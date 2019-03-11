0576f2598f948abcd8ed715345b91643c99d9834
bellom <bellomsean@gmail.com>
2019-03-11 16:03:32 +0100

    working with finebase

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-window.alert("good");
+var text = document.getElementById("text");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick(){
+    let firebaseRef = firebase.database().ref();
+    firebaseRef.child("Text").set("some names");
+}
